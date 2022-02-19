d63b87e68afadfc47ea59934a2e26d66cd2f0b50
ali abozaid <ali.abozaid22@gmail.com>
2022-02-19 15:48:55 +0100

    remove location request val

--- feature/map/src/main/kotlin/app/tier/map/presentation/TierMapFragment.kt
+++ feature/map/src/main/kotlin/app/tier/map/presentation/TierMapFragment.kt
@@ -1,9 +1,7 @@
 package app.tier.map.presentation
 
-import android.Manifest
 import android.os.Bundle
 import android.view.View
-import androidx.activity.result.contract.ActivityResultContracts
 import androidx.fragment.app.Fragment
 import app.tier.map.R
 import com.google.android.gms.maps.GoogleMap
@@ -14,22 +12,6 @@ class TierMapFragment : Fragment(R.layout.fragment_tier_map) {
 // 	private val binding by viewBinding(FragmentTierMapBinding::bind)
     private var googleMap: GoogleMap? = null
     private var mapFragment: SupportMapFragment? = null
-
-    val locationPermissionRequest = registerForActivityResult(
-        ActivityResultContracts.RequestMultiplePermissions()
-    ) { permissions ->
-        when {
-            permissions.getOrDefault(Manifest.permission.ACCESS_FINE_LOCATION, false) -> {
-                // Precise location access granted.
-            }
-            permissions.getOrDefault(Manifest.permission.ACCESS_COARSE_LOCATION, false) -> {
-                // Only approximate location access granted.
-            } else -> {
-                // No location access granted.
-            }
-        }
-    }
-
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
